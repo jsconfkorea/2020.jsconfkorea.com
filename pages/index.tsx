68f5889c5504eed404332a72a9f307a0f32a3151
Chris Yang <ysm0622@gmail.com>
2019-12-15 01:07:42 +0800

    Add open graph

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+export default () => (
+  <>
+    <style global jsx>{`
+      html,
+      body {
+        height: 100%;
+        margin: 0;
+      }
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2rem;
+      }
+    `}</style>
+    Comming soon...!
+  </>
+)
