c9e15f4569752f43b0e600642e257f2939367a3a
ysm0622 <ysm0622@gmail.com>
2020-05-20 21:30:46 +0900

    Edit ChangeLanguageLink to shallow routing

--- src/components/ChangeLangaugeLink.tsx
+++ src/components/ChangeLangaugeLink.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { ReactNode } from 'react'
 import { useRouter } from 'next/dist/client/router'
+import { useI18n } from '../hooks/useI18n'
 
 type Props = {
   lang: string
@@ -9,10 +10,17 @@ type Props = {
 
 const ChangeLanguageLink = ({ lang, children }: Props) => {
   const { asPath, pathname } = useRouter()
+  const { locale } = useI18n()
   const isLangPath = pathname.slice(0, 7) === '/[lang]'
   return (
-    <Link href={`/${lang}${isLangPath ? asPath.slice(3) : asPath === '/' ? '' : asPath}`}>
-      <a>{children}</a>
+    <Link href={`/${lang}${isLangPath ? asPath.slice(3) : asPath === '/' ? '' : asPath}`} shallow>
+      <a
+        onClick={(e) => {
+          locale(lang)
+        }}
+      >
+        {children}
+      </a>
     </Link>
   )
 }
