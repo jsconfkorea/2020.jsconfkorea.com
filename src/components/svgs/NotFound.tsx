afd2a44cf698af399ae37bcdfb555ab213f2933a
ysm0622 <ysm0622@gmail.com>
2020-06-07 13:19:08 +0900

    Convert svgs to React components

---
+++ src/components/svgs/NotFound.tsx
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+const NotFound = () => (
+  <svg
+    css={style}
+    version="1.1"
+    id="레이어_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 449.6 325.1"
+    style={{ background: 'new 0 0 449.6 325.1' }}
+    xmlSpace="preserve"
+  >
+    <g>
+      <polygon className="st0" points="247.4,0 340.7,93.1 154.1,93.1 	" />
+    </g>
+    <g>
+      <rect x="138.6" y="186.2" className="st0" width="46.5" height="93.1" />
+    </g>
+    <g>
+      <path
+        className="st1"
+        d="M154.4,93.3V186h46.5c0-25.6,20.8-46.4,46.4-46.4c25.6,0,46.4,20.8,46.4,46.4h0.1v93.3h46.5V186V93.3H154.4z"
+      />
+    </g>
+    <g>
+      <rect x="123" y="278.6" className="st1" width="93.1" height="46.5" />
+    </g>
+    <g>
+      <rect x="247.5" y="278.6" className="st0" width="93.1" height="46.5" />
+    </g>
+    <g>
+      <polygon className="st0" points="408.8,121.8 449.6,162.5 368.1,162.5 	" />
+    </g>
+    <g>
+      <rect x="368.1" y="243.5" className="st0" width="81.6" height="81.6" />
+    </g>
+    <g>
+      <rect x="355.8" y="162.5" className="st1" width="81.6" height="81.6" />
+    </g>
+    <g>
+      <polygon className="st0" points="81.6,162.3 81.6,243.8 0,243.8 	" />
+    </g>
+    <g>
+      <rect x="18.1" y="243.2" className="st1" width="81.6" height="81.6" />
+    </g>
+  </svg>
+)
+const style = css`
+  .st0 {
+    fill: #aaaaaa;
+  }
+  .st1 {
+    fill: #cccccc;
+  }
+`
+
+export default NotFound
