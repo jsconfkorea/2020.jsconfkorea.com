63e6cf747fd3fccd46f3d4078803baa3ed999cc9
거북이손가락 <turtlefingers@geobug-isongalag-ui-MacBookPro.local>
2020-06-07 13:56:58 +0900

    Change Intro Graphic / Change Styles

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -1,45 +1,47 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { useState, useEffect } from 'react'
-import ReactDOM from "react-dom";
 import useInterval from '../hooks/useInterval'
-import * as THREE from "three";
 
 const Graphic = () => {
+  const [count, setCount] = useState(0)
+  const [dCount, setDCount] = useState(0)
+  const [nextInterval, setInterval] = useState(6500)
 
-  useEffect(() => {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var container = document.getElementById('graphic');
-    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    if (container) {
-      container.innerHTML = "";
-      container.appendChild(renderer.domElement);
-    }
+  useInterval(() => {
+    let c = count + 1;
+    let d = (dCount + 1) % 7;
+    if (d == 0) setInterval(2000);
+    else setInterval(200);
+    setDCount(d);
+    if (c >= 10) c = 0;
+    setCount(c)
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0xff7235 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 3;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
-
-  });
-
+  }, nextInterval)
   return (
     <>
-      <div id="graphic"></div>
+      <div id="graphic" css={style}>
+        <img src={"/images/intro_" + count + ".svg"}></img>
+      </div>
     </>
   )
 }
 
-const style = css``
+const style = css`
+  height: 100%;
+  img{
+    width:300px;
+    height:auto;
+    position:absolute;
+    left:50%;
+    top:50%;
+    transform:translate3d(-50%,-50%,0);
+  }
+  @media screen and (max-width:768px){
+    img{
+      width:50%;
+    }
+  }
+`
 
 export default Graphic
