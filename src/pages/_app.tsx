6ea1b8d66007eb36a7e8eff60732ef0a7c25348c
ysm0622 <ysm0622@gmail.com>
2020-06-08 01:31:58 +0900

    change order meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,19 +19,19 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="keywords" content="JSConf, JavaScript, Conference" />
         <meta name="author" content="JSConf Korea" />
 
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={thumb} />
-        <meta property="og:site_name" content="JSConf Korea 2020" />
-        <meta property="og:url" content="https://2020.jsconfkorea.com" />
-        <meta property="og:type" content="website" />
-
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={thumb} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@jsconfkorea" />
         <meta name="twitter:creator" content="@jsconfkorea" />
+
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={thumb} />
+        <meta property="og:site_name" content="JSConf Korea 2020" />
+        <meta property="og:url" content="https://2020.jsconfkorea.com" />
+        <meta property="og:type" content="website" />
 
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
